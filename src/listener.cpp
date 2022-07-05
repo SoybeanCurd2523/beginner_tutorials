fc56d2255a34530575840b29e506222569ace336
jehyeon park <super3714@naver.com>
2022-07-05 14:45:22 +0900

    july 5

---
+++ src/listener.cpp
@@ -0,0 +1,20 @@
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+
+void chatterCallback(const std_msgs::Int32::ConstPtr& msg)
+{
+  ROS_INFO("I heard: [%d]", msg->data);
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "listener");
+
+  ros::NodeHandle nh;
+
+  ros::Subscriber sub = nh.subscribe("chatter", 1000, chatterCallback);
+
+  ros::spin();
+
+  return 0;
+}
---
+++ src/talker.cpp
@@ -0,0 +1,30 @@
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+
+int main(int argc, char **argv)
+{ 
+
+  ros::init(argc, argv, "talker");
+
+  ros::NodeHandle nh;
+
+  ros::Publisher chatter_pub = nh.advertise<std_msgs::Int32>("chatter", 1000);
+
+  ros::Rate loop_rate(1);
+
+  int count = 0;
+  while (ros::ok())
+  {
+    std_msgs::Int32 msg;
+    msg.data = count;
+    ROS_INFO("I speak : [%d]", msg.data);
+    chatter_pub.publish(msg); 
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+
+  return 0;
+}
